6e0ed6ebad1c4c841908ed91cc264fbb506c6f70
alex-bonetti <62677325+alex-bonetti@users.noreply.github.com>
2020-04-26 09:45:24 -0300

    Reacriação do projeto usando styled-components

--- components/screens/await.js
+++ components/screens/await.js
@@ -1,40 +1,32 @@
 import React, {useEffect} from "react";
 
-import {View, Text, StyleSheet, Image} from 'react-native';
+import styled from "styled-components/native";
 
-import firebase from '../firebaseConnection';
+import firebase from "../shared/firebaseConnection";
+
+const Page = styled.SafeAreaView`
+    flex:1;
+    align-items:center;
+    justify-content:center;
+`;
+
+const TextAlert = styled.Text`
+    font-size:25px;
+    text-align:center;
+    font-weight:center;
+`;
 
 const Await = ({navigation}) => {
     useEffect(()=>{
         firebase.auth().onAuthStateChanged((user)=>{
-            if(user){
-                navigation.navigate("TelaInicial");
-            }else{
-                navigation.navigate("Home");
-            }
+            user ? navigation.navigate("TelaInicial") : navigation.navigate("TelaInicialCadastro");
         })
-    })
+    }, []);
     return(
-        <View style={e.body}>
-            <Image source={require('../img/logo.png')} />
-            <Text style={e.loading}>Loading...</Text>
-        </View>
+        <Page>
+            <TextAlert>Verificando login...</TextAlert>
+        </Page>
     )
-}
-
-const e = StyleSheet.create({
-    body:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",
-        padding:10
-    },
-    loading:{
-        textAlign:"center",
-        fontSize:30,
-        fontWeight:"bold",
-        paddingTop:20
-    }
-})
+}   
 
 export default Await;
--- routes/Stack.js
+++ routes/Stack.js
@@ -1,31 +1,16 @@
 import React from "react";
-
 import {createStackNavigator} from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-import Cadastro from "../components/screens/cadastro";
-import Home from "../components/screens/home";
 import Login from "../components/screens/login";
+import Cadastro from '../components/screens/cadastro';
 import Await from "../components/screens/await";
-import TelaInicial from '../components/screens/telaInicial';
-import MoreInfo from "../components/screens/moreInfo";
-import AdicionarTarefa from "../components/screens/adicionarTarefa";
-import AttTarefa from "../components/screens/attTarefa";
 
-const myStack = () => {
-    return(
-        <Stack.Navigator initialRouteName="Await" screenOptions={{headerShown:false}}>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Cadastro" component={Cadastro} />
-            <Stack.Screen name='Login' component={Login} />
-            <Stack.Screen name="Await" component={Await} />
-            <Stack.Screen name="TelaInicial" component={TelaInicial} />
-            <Stack.Screen name="MoreInfo" component={MoreInfo} />
-            <Stack.Screen name="AdicionarTarefa" component={AdicionarTarefa} />
-            <Stack.Screen name="AttTarefa" component={AttTarefa} />
-        </Stack.Navigator>
-    )
+export default StackRoute = () => {
+    <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Await" component={Await} />
+    </Stack.Navigator>
 }
-
-export default myStack;
